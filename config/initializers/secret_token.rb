6d482e4690a6edafe5c43b71082efeb608ed2095
Joshua Sucsy <jsu@falconfone.com>
2011-01-19 17:00:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = 'f7a82961db615413fc88482a3210bf662751e2a25d53f89890473f81dba558498d10978252d1471e6b7a89b1bfbdef52f288900d1ba904cfa955ca04fa3b5ce4'
